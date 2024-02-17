60a45881ac07318255839ac5fe8b4cd5168e813c
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-17 15:11:58 +0530

    adding motion div

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./sidebar.scss"
+import Links from './links/Links'
+import ToggleButton from './toggleButton/ToggleButton'
+import { motion } from "framer-motion"
 
 export default function Sidebar() {
+
+    const [open, setOpen] = useState(false);
+
+    const varients = {
+        open: {
+            clipPath: "circle(1200px at 50px 50px)",
+            transition:{
+                type: "spring",
+                stiffness: 20,
+            }
+        },
+        closed: {
+            clipPath: "circle(30px at 50px 50px)",
+            transition : {
+                delay : 0.5,
+                type: "spring",
+                stiffness: 400,
+                damping: 40,
+            }
+        }
+    }
   return (
-    <div className="sidebar">
-        <div className="bg">
-            Links
-        </div>
-    </div>
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+        <motion.div className="bg" variants={varients}>
+            <Links />
+        </motion.div>
+        <ToggleButton setOpen={setOpen}/>
+    </motion.div>
   )
 }
