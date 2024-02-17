46154153b156e298c1bf1abbff8d2bcc248b618c
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-17 13:50:38 +0530

    adding Navbar in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import './app.scss'
+import Navbar from './components/navbar/Navbar';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar />
+    </section>
+    <section>Hero</section>
+  </div>;
 };
 
 export default App;
