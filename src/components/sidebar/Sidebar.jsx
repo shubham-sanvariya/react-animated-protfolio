b2fa18335a2ff6855e604b74afe268d709edcced
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-17 14:04:31 +0530

    adding Sidebar component

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./sidebar.scss"
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="bg">
+            Links
+        </div>
+    </div>
+  )
+}
