ec082d2ff2214ee9c7da23f4a603110bb63cf839
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-18 14:10:43 +0530

    adding textVarients and sliderVarients motion

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,73 @@
 import "./hero.scss"
+import { motion } from "framer-motion"
+
+
+const textVariants = {
+  initial: {
+    x:-500,
+    opacity:0,
+  },
+  animate: {
+    x : 0,
+    opacity : 1,
+    transition : {
+      duration : 1,
+      staggerChildren : 0.1
+    }
+  },
+  scrollButton: {
+    opacity : 0,
+    y : 10,
+    transition : {
+      duration : 2,
+      repeat : Infinity
+    }
+  }
+}
+
+const sliderVariants = {
+  initial: {
+    x:0,
+  },
+  animate: {
+    x : "-220%",
+    opacity : 1,
+    transition : {
+      repeat : Infinity,
+      repeatType : "mirror",
+      duration : 20,
+    }
+  }
+}
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>SHUBHAM SANWARIYA</h2>
-          <h1>Java-React Full Stack devloper</h1>
-          <div className="buttons">
-            <button>See the Latest Works</button>
-            <button>Contact Me</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
+        <motion.div className="textContainer"
+         variants={textVariants}
+         initial="initial"
+         animate="animate"
+         >
+          <motion.h2 variants={textVariants}>SHUBHAM SANWARIYA</motion.h2>
+          <motion.h1 variants={textVariants}>Java-React Full Stack devloper</motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>See the Latest Works</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img src="/scroll.png" alt=""
+           variants={textVariants}
+           animate="scrollButton"
+           />
+        </motion.div>
       </div>
-      <div className="slidingTextContainer">
+      <motion.div className="slidingTextContainer"
+       variants={sliderVariants}
+       initial="intial"
+       animate="animate"
+       >
         Love Coding Building Projects
-      </div>
+      </motion.div>
       <div className="imagecontainer">
         <img src="/hero.png" alt="" />
       </div>
