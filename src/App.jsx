4f0409f33cf02cae01169514589a56c0c90a03b1
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-19 21:54:45 +0530

    routing Portfolio in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,28 @@ import './app.scss'
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
+import Portfolio from './components/portfolio/Portfolio';
 import Services from './components/services/Services';
 
 const App = () => {
   return <div>
-    <section id="HomePage">
+    <section id="Homepage">
       <Navbar />
       <Hero />
     </section>
-    <section id='Service'><Parallax type="services"/></section>
-    <section><Services /></section>
-    <section id='Portfolio'><Parallax type="portfolio" /></section>
-    <section id='Contact'>Contact</section>
-    <section id='About'>About</section>
+    <section id="Services">
+      <Parallax type="services" />
+    </section>
+    <section>
+      <Services />
+    </section>
+    <section id="Portfolio">
+      <Parallax type="portfolio" />
+    </section>
+    <Portfolio />
+    <section id="Contact">
+      Contact
+    </section>
   </div>;
 };
 
