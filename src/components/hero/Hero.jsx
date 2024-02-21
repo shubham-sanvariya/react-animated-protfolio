2b570b581225cb662124fe70f137cbc6e0381048
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-21 15:14:15 +0530

    adding on click for buttons

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,38 +4,38 @@ import { motion } from "framer-motion"
 
 const textVariants = {
   initial: {
-    x:-500,
-    opacity:0,
+    x: -500,
+    opacity: 0,
   },
   animate: {
-    x : 0,
-    opacity : 1,
-    transition : {
-      duration : 1,
-      staggerChildren : 0.1
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1
     }
   },
   scrollButton: {
-    opacity : 0,
-    y : 10,
-    transition : {
-      duration : 2,
-      repeat : Infinity
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity
     }
   }
 }
 
 const sliderVariants = {
   initial: {
-    x:0,
+    x: 0,
   },
   animate: {
-    x : "-220%",
-    opacity : 1,
-    transition : {
-      repeat : Infinity,
-      repeatType : "mirror",
-      duration : 20,
+    x: "-220%",
+    opacity: 1,
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
     }
   }
 }
@@ -45,27 +45,31 @@ const Hero = () => {
     <div className="hero">
       <div className="wrapper">
         <motion.div className="textContainer"
-         variants={textVariants}
-         initial="initial"
-         animate="animate"
-         >
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
           <motion.h2 variants={textVariants}>SHUBHAM SANWARIYA</motion.h2>
           <motion.h1 variants={textVariants}>Java-React Full Stack devloper</motion.h1>
           <motion.div className="buttons" variants={textVariants}>
-            <motion.button variants={textVariants}>See the Latest Works</motion.button>
-            <motion.button variants={textVariants}>Contact Me</motion.button>
+            <motion.a href="#Portfolio">
+              <motion.button variants={textVariants}>See the Latest Works</motion.button>
+            </motion.a>
+            <motion.a href="#Contact">
+              <motion.button variants={textVariants}>Contact Me</motion.button>
+            </motion.a>
           </motion.div>
           <motion.img src="/scroll.png" alt=""
-           variants={textVariants}
-           animate="scrollButton"
-           />
+            variants={textVariants}
+            animate="scrollButton"
+          />
         </motion.div>
       </div>
       <motion.div className="slidingTextContainer"
-       variants={sliderVariants}
-       initial="intial"
-       animate="animate"
-       >
+        variants={sliderVariants}
+        initial="intial"
+        animate="animate"
+      >
         Love Coding Building Projects
       </motion.div>
       <div className="imagecontainer">
