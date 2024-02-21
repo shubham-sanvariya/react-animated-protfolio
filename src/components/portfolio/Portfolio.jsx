d0856f9a55e2d94027f7dc621349b8c318b8949c
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-21 15:13:59 +0530

    adding github links of projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,6 +8,7 @@ const items = [
     id: 1,
     title: "SpringFramework lakeSide Hotel",
     img: "./springlogo.webp",
+    link: "https://github.com/shubham-sanvariya/Spring_lake_side_hotel_backend",
     desc: `Welcome to SpringFramework LakeSide Hotel,
      an elegant and efficient hotel booking site designed
       to elevate the hospitality experience. This project
@@ -27,6 +28,7 @@ const items = [
     id: 2,
     title: "SpringFramework Pet Clinic",
     img: "./springlogo.webp",
+    link: "https://github.com/shubham-sanvariya/SpringFramework-Pet-Clinic",
     desc: `In my portfolio, I present a significant project
      developed using the Spring Framework, known as the Spring 
      Pet Clinic. This project encapsulates a sophisticated
@@ -49,6 +51,7 @@ One of the noteworthy aspects of this project is the utilization
     id: 3,
     title: "SpringFramework recipe app",
     img: "./springlogo.webp",
+    link: "https://github.com/shubham-sanvariya/spring5-recipe-app",
     desc: `Within my portfolio, I proudly present my Spring Framework Recipe App, a dynamic web application designed to ignite culinary creativity and streamline recipe management. This project showcases my proficiency in Java development, Spring Framework, and database management, along with my passion for creating intuitive and efficient software solutions.
 
 At the heart of this project lies an array of meticulously crafted models tailored to specific tasks. These models, including Difficulty, Category, Ingredients, Notes, and Recipe, form the backbone of the application, enabling users to seamlessly create, organize, and discover recipes.`
@@ -57,6 +60,7 @@ At the heart of this project lies an array of meticulously crafted models tailor
     id: 4,
     title: "SpringFramework Excel To Database",
     img: "./springlogo.webp",
+    link: "https://github.com/shubham-sanvariya/Spring-Framework-excel-to-database",
     desc: `In my portfolio, I showcase a cutting-edge project that exemplifies my proficiency in Spring Framework and database integration – the Spring Framework Excel to Database solution. This project demonstrates my ability to leverage advanced technologies to streamline data management processes, enhance efficiency, and ensure seamless interoperability between disparate systems.
 
 At the core of this project is the seamless transfer of data from Excel files to a MySQL database, facilitated by the Apache POI-OOXML dependency. Through meticulous implementation and integration with Spring Framework, I developed a robust and reliable solution that automates the extraction, transformation, and loading (ETL) process, minimizing manual effort and reducing the risk of errors.`
@@ -84,7 +88,9 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <button>see demo</button>
+            </a>
           </motion.div>
         </div>
       </div>
